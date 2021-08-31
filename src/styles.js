2ed4a7136eab7eeefd0a7d8dc155b503ac9d01c4
StayPerfect <udovyk@gmail.com>
2021-08-31 19:22:36 +0300

    z-index to styles

--- src/styles.js
+++ src/styles.js
@@ -13,6 +13,7 @@ export const StyledLayout = styled.div`
 export const StyledDescription = styled.div`
     flex: 1;
     padding-right: 5 rem;
+    z-index: 2;
 
     h2 {
         font-weight: lighter;
@@ -22,6 +23,7 @@ export const StyledDescription = styled.div`
 export const StyledImageDiv = styled.div`
     flex: 1;
     overflow: hidden;
+    z-index: 2;
 
     img {
         width: 100%;
