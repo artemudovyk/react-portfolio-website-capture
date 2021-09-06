25b70fd28b4d5f3386b55af88d6a8eb4827b37ae
StayPerfect <udovyk@gmail.com>
2021-09-06 12:32:01 +0300

    Added nav line + animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,53 @@
 import styled from 'styled-components/macro';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 function Nav() {
+    const { pathname } = useLocation();
+
     return (
         <StyledNav>
             <h1>
-                <Link id="logo" to="/">Capture</Link>
+                <Link id="logo" to="/">
+                    Capture
+                </Link>
             </h1>
             <ul>
                 <li>
                     <Link to="/">1. About Us</Link>
+                    <StyledNavLine
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: '0%', opacity: 0 }}
+                        animate={{
+                            width: pathname === '/' ? '50%' : '0%',
+                            opacity: pathname === '/' ? 1 : 0,
+                        }}
+                    />
                 </li>
 
                 <li>
                     <Link to="/work">2. Our Work</Link>
+                    <StyledNavLine
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: '0%', opacity: 0 }}
+                        animate={{
+                            width: pathname === '/work' ? '50%' : '0%',
+                            opacity: pathname === '/work' ? 1 : 0,
+                        }}
+                    />
                 </li>
 
                 <li>
                     <Link to="/contact">3. Contact Us</Link>
+                    <StyledNavLine
+                        transition={{ duration: 0.75 }}
+                        initial={{ width: '0%', opacity: 0 }}
+                        animate={{
+                            width: pathname === '/contact' ? '50%' : '0%',
+                            opacity: pathname === '/contact' ? 1 : 0,
+                        }}
+                    />
                 </li>
             </ul>
         </StyledNav>
@@ -48,13 +78,46 @@ const StyledNav = styled.nav`
 
     #logo {
         font-size: 1.5rem;
-        font-family: "Lobster", cursive;
+        font-family: 'Lobster', cursive;
         font-weight: lighter;
     }
 
     li {
         padding-left: 10rem;
         position: relative;
+    }
+
+    @media (max-width: 1300px) {
+        flex-direction: column;
+        padding: 1rem;
+
+        #logo {
+            display: inline-block;
+            margin: 0.5rem;
+        }
+
+        ul {
+            padding: 2rem;
+            justify-content: space-around;
+            width: 100%;
+
+            li {
+                padding: 0;
+            }
+        }
+    }
+`;
+
+const StyledNavLine = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+
+    @media (max-width: 1300px) {
+        left: 0%;
     }
 `;
 
